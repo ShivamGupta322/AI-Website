bf4734b051d36582b1ab616b1f919923f6e6192d
ShivamGupta322 <guptashivamsg02@gmail.com>
2024-07-08 21:00:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,61 @@
+var nm=document.querySelector("#name")
+var em=document.querySelector("#email")
+
+var msg=document.querySelector("#message")
+
+var cmp=document.querySelector("#company")
+
+var sub=document.querySelector("#submit")
+
+
+nm.addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.04)"; 
+    this.style.backgroundColor="#D3D3D3"
+  });
+  
+  nm.addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)"; 
+    this.style.backgroundColor="#fff"
+  });
+
+
+
+  em.addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.04)"; 
+    this.style.backgroundColor="#D3D3D3"
+  });
+  
+  em.addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)"; 
+    this.style.backgroundColor="#fff"
+  });
+
+
+  msg.addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.04)"; 
+    this.style.backgroundColor="#D3D3D3"
+  });
+  
+  msg.addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)"; 
+    this.style.backgroundColor="#fff"
+  });
+
+
+  cmp.addEventListener("mouseover", function() {
+    this.style.transform = "scale(1.04)"; 
+    this.style.backgroundColor="#D3D3D3"
+  });
+  
+  cmp.addEventListener("mouseout", function() {
+    this.style.transform = "scale(1)"; 
+    this.style.backgroundColor="#fff"
+  });
+
+
+
+
+
+
+
+ 
